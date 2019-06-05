44403942dff1a9cf83b2f0c27cf53a4e25a30bb2
Пользователь <you@example.com>
2019-06-05 22:32:28 +0300

    Создал проект в котором разделил код на отдельные файлы

---
+++ Paint.cpp
@@ -0,0 +1,20 @@
+#include "Paint.h"
+#include <iostream>
+
+using namespace std;
+
+void primer()
+{
+	cout<<"-1-|-2-|-3-"  <<endl;
+	cout<<"-4-|-5-|-6-"  <<endl;
+	cout<<"-7-|-8-|-9-"  <<endl;
+}
+void igrovaya_setka(char* mas)
+{
+ 	cout<< "\n";
+ 	cout<<"-"<<mas[0]<<"-|-"<<mas[1]<<"-|-"<<mas[2]<<"-"  <<endl;
+	cout<<"-"<<mas[3]<<"-|-"<<mas[4]<<"-|-"<<mas[5]<<"-"  <<endl;
+	cout<<"-"<<mas[6]<<"-|-"<<mas[7]<<"-|-"<<mas[8]<<"-"  <<endl;
+	cout<<"\n";
+ 
+}
